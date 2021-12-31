8edf85f3027860bc5e8704ef5eeb8e9b58d27bd3
Corolan <piotrekgajcy@wp.pl>
2021-12-31 15:37:47 +0100

    Added receiving mesages from APP

--- kod_Arduino/hovercraft_RC_control/hovercraft_RC_control.ino
+++ kod_Arduino/hovercraft_RC_control/hovercraft_RC_control.ino
@@ -4,13 +4,24 @@
 
 //TX-pin9, RX-pin8, pin10 - no PWM
 AltSoftSerial hc05;
-LiquidCrystal_I2C lcd(0x27, 16, 4); // set the LCD address to 0x27 for a 16 chars and 4 line display -- zmienić na wyswietlacz 16/4
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 4 line display -- zmienić na wyswietlacz 16/4
 
-const int received_data_size{3};
-int received_data[received_data_size];
+const int received_data_size {1}; //jak na razie przesyłam jeden bajt użytecznych danych
+int received_data[received_data_size + 2]; //"+2" - dochodzą znaki początku i końca ramki
+
+const int to_send_data_size{1};//jak na razie przesyłam jeden bajt użytecznych danych
+int data_to_send[to_send_data_size + 2];//"+2" - dochodzą znaki początku i końca ramki
+
+//pin A0 - odczyt napięcia zasilania
+float ps_voltage{0};//power supply voltage
 
 // DEBUG
 bool DEBUG = true;
+
+//deklaracje zmiennych sterujących
+bool LED_REFLEKTOR{false};
+
+void headlight_LED(bool state);
 
 void setup() {
   if (DEBUG == true) {
@@ -20,7 +31,8 @@ void setup() {
     lcd.setCursor(0, 2);
     lcd.print("----LET'S START!----");
     Serial.begin(9600);
-    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);//DEBUG
+    pinMode(13, OUTPUT); digitalWrite(13, LOW); //DEBUG
   }
   hc05.begin(9600);
 }
@@ -37,14 +49,41 @@ void loop() {
       lcd.print(received_data[i]); lcd.print("-");
       i++;
     }
-    if (received_data[0] == '@' && received_data[received_data_size-1] == '#'){
-      //pełna ramka odebrana
+    if (received_data[0] == 64 && received_data[received_data_size + 1] == 35) { // Sprawdzenie kompletności odebranych danych, '@'=64, '#'=35
+      //pełna ramka odebrana, tutaj będzie procesowanie rozkazów
       lcd.setCursor(0, 3); lcd.print("Data detected");
+      lcd.setCursor(0, 2); lcd.print(received_data[1]);
+      LED_REFLEKTOR = received_data[1];
     }
 
   }
 
-  //digitalWrite(13, HIGH);
+  //Wyonanie rozkazów na podstawie otrzymanych danych
+  headlight_LED(LED_REFLEKTOR);
 
+  //Przygotowanie danych do wysłania
+  //1. pomiar napięcia zasilania
+  ps_voltage = read_voltage();
+
+  //Wysyłanie danych do aplikacji
+  data_to_send[1] = ps_voltage
+  
+  
+  //czy delay konieczny?
   delay(100);
+}
+
+float read_voltage(){
+  //po odjeciu lcd przerobićto na pojedynczy return
+  float voltage = analogRead(A0)*5/1023.0;//przeliczenie na wolty
+  lcd.setCursor(15, 0); lcd.print(voltage);
+  return voltage;
+}
+
+void headlight_LED(bool state) {
+  if (state) {
+    digitalWrite(11, HIGH);
+  } else {
+    digitalWrite(11, LOW);
+  }
 }
