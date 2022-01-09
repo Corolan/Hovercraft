a46a2d9f33701a16856df2f2b5b2064d5434fe10
Corolan <piotrekgajcy@wp.pl>
2022-01-09 18:46:36 +0100

    Clean up - part 1

--- kod_Arduino/hovercraft_RC_control/hovercraft_RC_control.ino
+++ kod_Arduino/hovercraft_RC_control/hovercraft_RC_control.ino
@@ -6,10 +6,14 @@
 AltSoftSerial hc05;
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 4 line display -- zmienić na wyswietlacz 16/4
 
-const int received_data_size {2}; //jak na razie przesyłam jeden bajt użytecznych danych
+const int received_data_size {
+  2
+}; //jak na razie przesyłam jeden bajt użytecznych danych
 int received_data[received_data_size + 3]; //"+2" - dochodzą znaki początku i końca ramki oraz checksuma
 
-const int to_send_data_size{3};
+const int to_send_data_size {
+  4
+};
 int data_to_send[to_send_data_size + 3];//"+3" - dochodzą znaki początku i końca ramki oraz suma kontrolna
 int checksum{0}; // policzyć sumę kontrolną
 
@@ -17,23 +21,25 @@ int checksum{0}; // policzyć sumę kontrolną
 #define OBSTCL_LEFT 3
 #define OBSTCL_RIGHT 2
 #define ENG_VERTICAL 4 //silnik pionowy ( dmchaw[a/y] )
+#define trigPin A5
+#define echoPin A4
 //silnik pin 4
 
 int ps_voltage{0};//power supply voltage
+
 
 // DEBUG
 bool DEBUG = true;
 byte test_variable{0};
 
-//deklaracje zmiennych 
+//deklaracje zmiennych
 bool LED_REFLEKTOR{false};
-byte obstacle_left{0};
-byte obstacle_right{0};
 bool vertical_engine{0};
 
 //deklaracje funkcji
 int read_voltage();
-void check_obstacles();
+int check_obstacles(int pin);
+int read_distance();
 
 void headlight_LED(bool state);
 void control_vertical_engines(bool state);
@@ -53,6 +59,7 @@ void setup() {
   pinMode(OBSTCL_LEFT, INPUT);
   pinMode(OBSTCL_RIGHT, INPUT);
   pinMode(ENG_VERTICAL, OUTPUT); digitalWrite(ENG_VERTICAL, LOW);
+  Serial.println("TEST");
 }
 
 void loop() {
@@ -62,11 +69,11 @@ void loop() {
     digitalWrite(ENG_VERTICAL, LOW);
     lcd.clear(); lcd.setCursor(0, 0); lcd.print("No data");
   } else { //pojawiły się rozkazy!
-    
+
     lcd.clear(); lcd.setCursor(0, 0);
     int i{0};
     while ( hc05.available() > 0) {
-      
+
       received_data[i] = hc05.read();
       lcd.print(received_data[i]); lcd.print("-");
       i++;
@@ -86,35 +93,36 @@ void loop() {
   control_vertical_engines(vertical_engine);
 
 
-  //Wysyłanie danych do aplikacji --- czy przenieść to do sekcji w drugim if-ie? Tak żeby próba nadania danych następowala tylko gdy po drugiej stronie jest aplikacja 
+  //Wysyłanie danych do aplikacji --- czy przenieść to do sekcji w drugim if-ie? Tak żeby próba nadania danych następowala tylko gdy po drugiej stronie jest aplikacja
   checksum = 0; //jaki algorym?
 
-  check_obstacles();
-  
+
+
   data_to_send[0] = 64; //'@';
-  data_to_send[to_send_data_size + 1] = checksum; 
+  data_to_send[to_send_data_size + 1] = checksum;
   data_to_send[to_send_data_size + 2] = 35; //'#';
   //Przygotowanie danych do wysłania
   data_to_send[1] = read_voltage();//1. pomiar napięcia zasilania
-  data_to_send[2] = obstacle_left;
-  data_to_send[3] = obstacle_right;
+  data_to_send[2] = check_obstacles(OBSTCL_LEFT); //;eft
+  data_to_send[3] = check_obstacles(OBSTCL_RIGHT);
+  data_to_send[3] = read_distance();
 
   for (int j{0}; j < to_send_data_size + 3; j++) {//"+3" bo checksum-a
     hc05.write(data_to_send[j]);
-    //Serial.print(data_to_send[j]);
-    //Serial.print("|");
+    Serial.print(data_to_send[j]);
+    Serial.print("|");
   }
   //Serial.println();
   //czy delay konieczny? zamienić go na millis?
   delay(200);
 }
 
-int read_voltage(){
+int read_voltage() {
   //po odjeciu lcd przerobić to na pojedynczy return
   //float voltage = analogRead(LED_VOLTAGE)*5/1023.0;//przeliczenie na wolty
   int voltage = analogRead(LED_VOLTAGE);//
-  lcd.setCursor(15, 0); lcd.print(voltage/4);
-  return voltage/4; // czynnik "4" wynika z tego, że muszę zmieścić się w jednym bajcie
+  //lcd.setCursor(15, 0); lcd.print(voltage / 4);
+  return voltage / 4; // czynnik "4" wynika z tego, że muszę zmieścić się w jednym bajcie
 }
 
 void headlight_LED(bool state) { //dodać definicję pinu headlight
@@ -125,24 +133,31 @@ void headlight_LED(bool state) { //dodać definicję pinu headlight
   }
 }
 
-void check_obstacles() {
-  if (digitalRead(OBSTCL_LEFT) == LOW) {
-    obstacle_left = 1;
+int check_obstacles(int pin) {
+  if (digitalRead(pin) == LOW) {
+    return 1;
   } else {
-    obstacle_left = 0;
-  }
-  if (digitalRead(OBSTCL_RIGHT) == LOW) {
-    obstacle_right = 1;
-  } else {
-    obstacle_right = 0;
+    return 0;
   }
 }
 
-void control_vertical_engines(bool state){
+void control_vertical_engines(bool state) {
   if (state == true) {
     digitalWrite(ENG_VERTICAL, HIGH);
   }
   else if (state == false) {
     digitalWrite(ENG_VERTICAL, LOW);
   }
+}
+
+int read_distance() {
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  Serial.println(echoPin, HIGH);
+  return 12;
+  return pulseIn(echoPin, HIGH);
 }
